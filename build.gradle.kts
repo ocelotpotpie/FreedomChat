abdb14a4c93fe136d0e0ce6060bd2854555af9c6
Christopher Bohn <pop4959@gmail.com>
2024-07-16 22:15:20 -0700

    Bump version for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ subprojects {
     plugins.apply("java-library")
 
     group = "ru.bk.oharass.freedomchat"
-    version = "1.6.1"
+    version = "1.7.0"
     description = "Liberate your server from the chat-reporting bourgeoisie! Disable chat signing server-side."
 
     tasks {
