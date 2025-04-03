0eadc368d2bb84c6823a9f597b7ac52b17373654
Christopher Bohn <pop4959@gmail.com>
2025-04-03 00:27:33 -0700

    revert bump

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -14,9 +14,9 @@ repositories {
 
 dependencies {
     minecraft(group = "com.mojang", name = "minecraft", version = "1.21.4")
-    mappings(group = "net.fabricmc", name = "yarn", version = "1.21.4+build.8", classifier = "v2")
-    modImplementation(group = "net.fabricmc", name = "fabric-loader", version = "0.16.12")
-    modImplementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "0.119.2+1.21.4")
+    mappings(group = "net.fabricmc", name = "yarn", version = "1.21.4+build.1", classifier = "v2")
+    modImplementation(group = "net.fabricmc", name = "fabric-loader", version = "0.16.9")
+    modImplementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = "0.111.0+1.21.4")
     shade(implementation(group = "org.spongepowered", name = "configurate-yaml", version = "4.1.2"))
 }
 
